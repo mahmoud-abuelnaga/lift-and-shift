571fc3656fb2211749933d35aced16ffb1a16cba
Mahmoud Abuelnaga <mahmoud.abuelnaga0@gmail.com>
2025-06-13 03:20:18 +0300

    commiting my code again

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -564,8 +564,403 @@ EOF
     echo "$template_id"
 }
 
+create_target_group() {
+    local tg_name="$1"
+    local forwarding_protocol="$2"
+    local traffic_port="$3"
+    local vpc_id="$4"
+    local target_type="${5:-instance}"
+    local health_check_protocol="${6:-HTTP}"
+    local health_check_port="${7:-$traffic_port}"
+    local health_check_path="${8:-/}"
+    local health_check_interval_seconds="${9:-30}"
+    local health_check_timeout_seconds="${10:-6}"
+    local healthy_threshold_count="${11:-5}"
+    local unhealthy_threshold_count="${12:-2}"
+
+    if [[ -z "$tg_name" ]]; then
+        echo "(internal error) 'create_target_group' is called with no tg_name" >&2
+        return 1
+    fi
+
+    if [[ -z "$forwarding_protocol" ]]; then
+        echo "(internal error) 'create_target_group' is called with no forwarding_protocol" >&2
+        return 1
+    fi
+
+    if [[ -z "$traffic_port" ]]; then
+        echo "(internal error) 'create_target_group' is called with no traffic_port" >&2
+        return 1
+    fi
+
+    if [[ -z "$vpc_id" ]]; then
+        echo "(internal error) 'create_target_group' is called with no vpc_id" >&2
+        return 1
+    fi
+
+    if [[ -z "$target_type" ]]; then
+        echo "(internal error) 'create_target_group' is called with no target_type" >&2
+        return 1
+    fi
+
+    if [[ -z "$health_check_protocol" ]]; then
+        echo "(internal error) 'create_target_group' is called with no health_check_protocol" >&2
+        return 1
+    fi
+
+    if [[ -z "$health_check_port" ]]; then
+        echo "(internal error) 'create_target_group' is called with no health_check_port" >&2
+        return 1
+    fi
+
+    if [[ -z "$health_check_path" ]]; then
+        echo "(internal error) 'create_target_group' is called with no health_check_path" >&2
+        return 1
+    fi
+
+    if [[ -z "$health_check_interval_seconds" ]]; then
+        echo "(internal error) 'create_target_group' is called with no health_check_interval_seconds" >&2
+        return 1
+    fi
+
+    if [[ -z "$health_check_timeout_seconds" ]]; then
+        echo "(internal error) 'create_target_group' is called with no health_check_timeout_seconds" >&2
+        return 1
+    fi
+
+    if [[ -z "$healthy_threshold_count" ]]; then
+        echo "(internal error) 'create_target_group' is called with no healthy_threshold_count" >&2
+        return 1
+    fi
+
+    if [[ -z "$unhealthy_threshold_count" ]]; then
+        echo "(internal error) 'create_target_group' is called with no unhealthy_threshold_count" >&2
+        return 1
+    fi
+
+    local tg_arn
+    tg_arn=$(aws elbv2 create-target-group \
+        --name "$tg_name" \
+        --protocol "$forwarding_protocol" \
+        --port "$traffic_port" \
+        --vpc-id "$vpc_id" \
+        --target-type "$target_type" \
+        --health-check-protocol "$health_check_protocol" \
+        --health-check-port "$health_check_port" \
+        --health-check-path "$health_check_path" \
+        --health-check-interval-seconds "$health_check_interval_seconds" \
+        --health-check-timeout-seconds "$health_check_timeout_seconds" \
+        --healthy-threshold-count "$healthy_threshold_count" \
+        --unhealthy-threshold-count "$unhealthy_threshold_count" \
+        --query 'TargetGroups[0].TargetGroupArn' \
+        --output text) || return "$?"
+
+    echo "$tg_arn"
+}
+
+create_load_balancer() {
+    local name="$1"
+    local scheme="$2"
+    local type="$3"
+    local ip_address_type="$4"
+    local security_groups_str="$5"
+    local subnets_str="$6"
+    
+    local security_groups
+    read -ra security_groups <<< "$security_groups_str"
+
+    local subnets
+    read -ra subnets <<< "$subnets_str"
+
+    if [[ -z "$name" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no name" >&2
+        return 1
+    fi
+
+    if [[ -z "$scheme" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no scheme" >&2
+        return 1
+    fi
+
+    if [[ -z "$type" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no type" >&2
+        return 1
+    fi
+
+    if [[ -z "$ip_address_type" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no ip_address_type" >&2
+        return 1
+    fi
+
+    if [[ -z "$security_groups_str" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no security_groups" >&2
+        return 1
+    fi
+
+    if [[ -z "$subnets_str" ]]; then
+        echo "(internal error) 'create_load_balancer' is called with no subnets" >&2
+        return 1
+    fi
+
+    local lb_arn
+    lb_arn=$(aws elbv2 create-load-balancer \
+        --name "$name" \
+        --scheme "$scheme" \
+        --type "$type" \
+        --ip-address-type "$ip_address_type" \
+        --security-groups "${security_groups[@]}" \
+        --subnets "${subnets[@]}" \
+        --query 'LoadBalancers[0].LoadBalancerArn' \
+        --output text) || return "$?"
+
+    echo "$lb_arn"
+}
+
+describe_vpc_subnets() {
+    local vpc_id="$1"
+    if [[ -z "$vpc_id" ]]; then
+        echo "(internal error) 'describe_vpc_subnets' is called with no vpc_id" >&2
+        return 1
+    fi
+
+    aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query "Subnets[*].SubnetId" --output text
+}
+
+create_load_balancer_listener() {
+    local lb_arn="$1"
+    local protocol="$2"
+    local port="$3"
+    local target_groupd_arn="$4"
+    local action="${5:-forward}"
+    local certificate_arn="$6"
+
+    if [[ -z "$lb_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_listener' is called with no lb_arn" >&2
+        return 1
+    fi
+
+    if [[ -z "$protocol" ]]; then
+        echo "(internal error) 'create_load_balancer_listener' is called with no protocol" >&2
+        return 1
+    fi
+
+    if [[ -z "$port" ]]; then
+        echo "(internal error) 'create_load_balancer_listener' is called with no port" >&2
+        return 1
+    fi
+
+    if [[ -z "$target_groupd_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_listener' is called with no target_groupd_arn" >&2
+        return 1
+    fi
+
+    local cmd
+    cmd="aws elbv2 create-listener \
+            --load-balancer-arn $lb_arn \
+            --protocol $protocol \
+            --port $port \
+            --default-actions Type=$action,TargetGroupArn=$target_groupd_arn \
+            --query 'Listeners[0].ListenerArn' \
+            --output text"
+
+    if [[ -n "$certificate_arn" ]]; then
+        cmd+=" --certificates CertificateArn=$certificate_arn"
+    fi
+
+    local listener_arn
+    listener_arn=$(eval "$cmd") || return "$?"
+
+    echo "$listener_arn"
+}
+
+create_load_balancer_http_listener() {
+    local lb_arn="$1"
+    local tg_arn="$2"
+    if [[ -z "$lb_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_http_listener' is called with no lb_arn" >&2
+        return 1
+    fi
+
+    if [[ -z "$tg_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_http_listener' is called with no tg_arn" >&2
+        return 1
+    fi
+
+    local listener_arn
+    listener_arn=$(create_load_balancer_listener "$lb_arn" "HTTP" "80" "$tg_arn") || return "$?"
+
+    echo "$listener_arn"
+}
+
+create_load_balancer_https_listener() {
+    local lb_arn="$1"
+    local tg_arn="$2"
+    local certificate_arn="$3"
+    if [[ -z "$lb_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_https_listener' is called with no lb_arn" >&2
+        return 1
+    fi
+
+    if [[ -z "$tg_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_https_listener' is called with no tg_arn" >&2
+        return 1
+    fi
+
+    if [[ -z "$certificate_arn" ]]; then
+        echo "(internal error) 'create_load_balancer_https_listener' is called with no certificate_arn" >&2
+        return 1
+    fi
+
+    local listener_arn
+    listener_arn=$(create_load_balancer_listener "$lb_arn" "HTTPS" "443" "$tg_arn" "forward" "$certificate_arn") || return "$?"
+
+    echo "$listener_arn"
+}
+
+create_autoscaling_group_with_elb_check() {
+    local asg_name="$1"
+    local launch_temp_name="$2"
+    
+    # shellcheck disable=SC2016
+    local launch_temp_version="${3:-'$Latest'}"
+    local min_size="${4:-1}"
+    local max_size="${5:-4}"
+    local desired_capacity="${6:-2}"
+    local subnets_str="$7"
+    local tg_arn="$8"
+    local health_check_grace_period="${9:-300}"
+
+    if [[ -z "$asg_name" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no asg_name" >&2
+        return 1
+    fi
+
+    if [[ -z "$launch_temp_name" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no launch_temp_name" >&2
+        return 1
+    fi
+
+    if [[ -z "$launch_temp_version" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no launch_temp_version" >&2
+        return 1
+    fi
+
+    if [[ -z "$min_size" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no min_size" >&2
+        return 1
+    fi
+
+    if [[ -z "$max_size" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no max_size" >&2
+        return 1
+    fi
+
+    if [[ -z "$desired_capacity" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no desired_capacity" >&2
+        return 1
+    fi
+
+    if [[ -z "$subnets_str" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no subnets_str" >&2
+        return 1
+    fi
+
+    if [[ -z "$tg_arn" ]]; then
+        echo "(internal error) 'create_autoscaling_group_with_elb_check' is called with no tg_arn" >&2
+        return 1
+    fi
+
+    echo "$subnets_str"
+    local subnets_arr
+    read -ra subnets_arr <<< "$subnets_str"
+    subnets_str=$(printf '%s,' "${subnets_arr[@]}")
+    echo "$subnets_str"
+    subnets_str=${subnets_str%,}
+    echo "$subnets_str"
+    
+    aws autoscaling create-auto-scaling-group \
+        --auto-scaling-group-name "$asg_name" \
+        --launch-template "LaunchTemplateName=$launch_temp_name,Version=$launch_temp_version" \
+        --min-size "$min_size" \
+        --max-size "$max_size" \
+        --desired-capacity "$desired_capacity" \
+        --vpc-zone-identifier "$subnets_str" \
+        --target-group-arns "$tg_arn" \
+        --health-check-type ELB \
+        --health-check-grace-period "$health_check_grace_period" || return "$?"
+}
+
+add_autoscaling_policy_for_asg() {
+    local asg_name="$1"
+    local policy_name="$2"
+    local policy_type="$3"
+    local tracking_conf="$4"
+
+    if [[ -z "$asg_name" ]]; then
+        echo "(internal error) 'add_autoscaling_policy_for_asg' is called with no asg_name" >&2
+        return 1
+    fi
+
+    if [[ -z "$policy_name" ]]; then
+        echo "(internal error) 'add_autoscaling_policy_for_asg' is called with no policy_name" >&2
+        return 1
+    fi
+
+    if [[ -z "$policy_type" ]]; then
+        echo "(internal error) 'add_autoscaling_policy_for_asg' is called with no policy_type" >&2
+        return 1
+    fi
+
+    if [[ -z "$tracking_conf" ]]; then
+        echo "(internal error) 'add_autoscaling_policy_for_asg' is called with no tracking_conf" >&2
+        return 1
+    fi
+
+    local policy_arn
+    policy_arn=$(aws autoscaling put-scaling-policy \
+        --auto-scaling-group-name "$asg_name" \
+        --policy-name "$policy_name" \
+        --policy-type "$policy_type" \
+        --target-tracking-configuration "$tracking_conf" \
+        --query 'PolicyARN' \
+        --output text) || return "$?"
+
+    echo "$policy_arn"
+}
+
+enable_tg_lb_stickness() {
+    local tg_arn="$1"
+    local stickness_duration="$2"
+
+    if [[ -z "$tg_arn" ]]; then
+        echo "(internal error) 'enable_tg_lb_stickness' is called with no tg_arn" >&2
+        return 1
+    fi
+
+    if [[ -z "$stickness_duration" ]]; then
+        echo "(internal error) 'enable_tg_lb_stickness' is called with no stickness_duration" >&2
+        return 1
+    fi
+
+    aws elbv2 modify-target-group-attributes \
+        --target-group-arn "$tg_arn" \
+        --attributes Key=stickiness.enabled,Value=true Key=stickiness.type,Value=lb_cookie Key=stickiness.lb_cookie.duration_seconds,Value="$stickness_duration" > "$DEV_NULL" || return "$?"
+}
+
+terminate_ec2_instances() {
+    if [[ $# -eq 0 ]]; then
+        echo "(internal error) 'terminate_ec2_instances' is called with no instance ids" >&2
+        return 1
+    fi
+
+    aws ec2 terminate-instances --instance-ids "$@" > "$DEV_NULL" || return "$?"
+}
+
+
 # configurable variables
 vpc_id="vpc-0c39a1bc674978444"
+vpc_subnets_id_str="$(describe_vpc_subnets "$vpc_id")"
+certificate_arn="arn:aws:acm:us-east-1:886436923743:certificate/137ee2d9-7899-4972-a3e5-689ef464cfa7"
 region="us-east-1"
 key_name="key-vprofile"
 key_path="./key-vprofile.pem"
@@ -577,7 +972,7 @@ mariadb_instance_name="ec2-mariadb"
 memcached_instance_name="ec2-memcached"
 rabbitmq_instance_name="ec2-rabbitmq"
 tomcat_instance_name="ec2-tomcat"
-tomcat_template_name=""
+tomcat_template_name="template-ubuntu-tomcat-webserver"
 mariadb_init_script_path="../vprofile-project/userdata/mysql.sh"
 memcached_init_script_path="../vprofile-project/userdata/memcache.sh"
 tomcat_server_init_script_path="../vprofile-project/userdata/tomcat_ubuntu.sh"
@@ -603,13 +998,15 @@ application_war_path="../vprofile-project/target/vprofile-v2.war"
 
 tomcat_service_name="tomcat10"
 
+asg_name="asg-ubuntu-tomcat-web-server"
+
 # create security groups
 ## load balancer security group
 # lb_secg_id=$(create_sg "$lb_secg_name" "$lb_secg_desc" "$vpc_id") || die "Failed to create security group $lb_secg_name" "$?"
 # allow_http_for_sg "$lb_secg_id" || die "Failed to allow HTTP for security group $lb_secg_id" "$?"
 # allow_https_for_sg "$lb_secg_id" || die "Failed to allow HTTPs for security group $lb_secg_id" "$?"
 
-# # ## web server security group
+## web server security group
 # web_server_secg_id=$(create_sg "$web_server_secg_name" "$web_server_secg_desc" "$vpc_id") || die "Failed to create security group $web_server_secg_name" "$?"
 # allow_incoming_traffic_on_port_from_another_sg "$web_server_secg_id" "tcp" 8080 "$lb_secg_id" || die "Failed to allow requests from load balancer security group for $web_server_secg_id" "$?"
 # allow_ssh_from_all_ips_for_sg "$web_server_secg_id" || die "Failed to allow ssh for $web_server_secg_name"
@@ -652,7 +1049,7 @@ tomcat_service_name="tomcat10"
 
 # update_hosted_zone_record_set "$hosted_zone_id" "$change_record_set_string" || die "failed to change hosted zone record set" "$?"
 
-# # # update the application.properties
+# update the application.properties
 # sed -i "s/^memcached\.active\.host=.*/mecached.active.host=$memcached_dns_record_name.$private_domain_name/" "$application_properites_path"
 # sed -i "s/^rabbitmq\.address=.*/rabbitmq.address=$rabbitmq_dns_record_name.$private_domain_name/" "$application_properites_path"
 # sed -i "s|^jdbc\.url=.*|jdbc.url=jdbc:mysql://$mariadb_dns_record_name.$private_domain_name:3306/accounts?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull|" "$application_properites_path"
@@ -660,26 +1057,60 @@ tomcat_service_name="tomcat10"
 # build the application
 # mvn -f "$application_build_path/pom.xml" clean install
 
-# # # create web server instance
+# create web server instance
 # tomcat_instance_id=$(create_ec2_instance "$ubuntu_2404_ami_id" "$default_instance_type" "$key_name" "$web_server_secg_id" "$tomcat_instance_name" 1 "$tomcat_server_init_script_path") || die "failed to create tomcat instance" "$?"
 # wait_till_ec2_instance_is_running "$tomcat_instance_id"
 # sleep 60
 
-# # add the server fingerprint to the know hosts
+# add the server fingerprint to the know hosts
 # tomcat_public_ip=$(get_instance_public_ip "$tomcat_instance_id") || die "failed to get tomcat server public ip" "$?"
 # echo "Ubuntu Tomcat Temporary Webserver Public IP: $tomcat_public_ip"
 # ssh_add_host_to_known_hosts "$tomcat_public_ip" || die "failed to add the tomcat web server fingerprint to the know hosts" "$?"
 
 # remove default tomcat ROOT folder
 # ssh -i "$key_path" "$ubuntu_ami_default_username"@"$tomcat_public_ip" "sudo systemctl stop $tomcat_service_name; sudo rm -rf /var/lib/tomcat10/webapps/ROOT"
-# # copy the build
+# copy the build
 # scp -i "$key_path" "$application_war_path" "$ubuntu_ami_default_username"@"$tomcat_public_ip":/tmp/vprofile.war || die "failed to copy the war file to the tomcat server" "$?"
-# # deploying app 2
+# deploying app 2
 # ssh -i "$key_path" "$ubuntu_ami_default_username"@"$tomcat_public_ip" "sudo systemctl stop $tomcat_service_name; sudo rm -rf /var/lib/tomcat10/webapps/ROOT; sudo mv /tmp/vprofile.war /var/lib/tomcat10/webapps/ROOT.war; sudo systemctl enable $tomcat_service_name; sudo systemctl start $tomcat_service_name" || die "failed to deploy the war file to the tomcat server" "$?"
 
-# # create image out of ubuntu tomcat webserver
+# create image out of ubuntu tomcat webserver
 # ubuntu_tomcat_webserver_image_id=$(create_image_from_ec2_instance "$tomcat_instance_id" "ubuntu-vprofile-tomcat-webserver" "Ubuntu Vprofile Tomcat Webserver") || die "failed to create image out of ubuntu tomcat webserver" "$?"
-# wait_till_image_is_available "$ubuntu_tomcat_webserver_image_id" || die "failed to wait till image is available" "$?"
 
 # create launch template out of the image id
-# create_launch_template "template-ubuntu-tomcat-webserver" "v1 of ubuntu tomcat web server" "$ubuntu_tomcat_webserver_image_id" "$default_instance_type" "$key_name" "ec2-ubuntu-tomcat-web-server" "$web_server_secg_id" || die "failed to create launch template out of the image id" "$?"
+# template_id=$(create_launch_template "$tomcat_template_name" "v1 of ubuntu tomcat web server" "$ubuntu_tomcat_webserver_image_id" "$default_instance_type" "$key_name" "ec2-ubuntu-tomcat-web-server" "$web_server_secg_id") || die "failed to create launch template out of the image id" "$?"
+
+# create target group
+# tg_arn=$(create_target_group "tg-ubuntu-tomcat-web-server" "HTTP" "8080" "$vpc_id") || die "failed to create target group" "$?"
+# enable_tg_lb_stickness "$tg_arn" "86400" || die "failed to enable tg lb stickness" "$?"
+
+# create application load balancer
+# lb_arn=$(create_load_balancer "lb-ubuntu-tomcat-web-server" "internet-facing" "application" "ipv4" "$lb_secg_id" "$vpc_subnets_id_str") || die "failed to create application load balancer" "$?"
+
+# create http listener
+# shellcheck disable=SC2034
+# http_listener_arn=$(create_load_balancer_http_listener "$lb_arn" "$tg_arn") || die "failed to create http listener" "$?"
+
+# create https listener
+# shellcheck disable=SC2034
+# https_listener_arn=$(create_load_balancer_https_listener "$lb_arn" "$tg_arn" "$certificate_arn") || die "failed to create https listener" "$?"
+
+# wait for image to be availabe
+# wait_till_image_is_available "$ubuntu_tomcat_webserver_image_id" || die "failed to wait till image is available" "$?"
+
+# delete original instance
+# terminate_ec2_instances "$tomcat_instance_id" || die "failed to terminate original instance" "$?"
+
+# create autoscaling group
+# shellcheck disable=SC2016
+# create_autoscaling_group_with_elb_check "$asg_name" "$tomcat_template_name" '$Latest' 1 4 2 "$vpc_subnets_id_str" "$tg_arn" || die "failed to create autoscaling group" "$?"
+# asp_arn=$(add_autoscaling_policy_for_asg "$asg_name" "cpu-utilization-policy" "TargetTrackingScaling" "$(cat << EOF
+# {
+#     "PredefinedMetricSpecification": {
+#         "PredefinedMetricType": "ASGAverageCPUUtilization"
+#     },
+#     "TargetValue": 50.0
+# }
+# EOF
+# )") || die "failed to add autoscaling policy for asg" "$?"
+
